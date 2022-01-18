60045969d795a1ec59ebc40fcced66373a74d8fb
anoriar <borisov.alex12@gmail.com>
2022-01-18 09:03:17 +0300

    music shop plus btn

--- src/main/java/com/example/musicshop/MainActivity.java
+++ src/main/java/com/example/musicshop/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void increaseQuantity(View view) {
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        this.quantity++;
+        quantityTextView.setText(String.valueOf(this.quantity));
     }
 }
