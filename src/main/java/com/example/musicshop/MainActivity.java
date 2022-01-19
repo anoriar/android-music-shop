7aa03ec7d2981a0a452f85282b916e48658bd75f
anoriar <borisov.alex12@gmail.com>
2022-01-19 08:56:19 +0300

    spinner

--- src/main/java/com/example/musicshop/MainActivity.java
+++ src/main/java/com/example/musicshop/MainActivity.java
@@ -4,21 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     public int quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        spinnerArrayList = new ArrayList();
+
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
     }
 
     public void increaseQuantity(View view) {
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         this.quantity++;
+        quantityTextView.setText(String.valueOf(this.quantity));
+    }
+
+    public void decreaseQuantity(View view) {
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        this.quantity--;
+        if(this.quantity < 0){
+            this.quantity = 0;
+        }
         quantityTextView.setText(String.valueOf(this.quantity));
     }
 }
