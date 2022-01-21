a9152650967c54d5bc1a62352658fc3f4fc7a2e5
anoriar <borisov.alex12@gmail.com>
2022-01-21 08:28:31 +0300

    image change in spinner + refactoring

--- src/main/java/com/example/musicshop/MainActivity.java
+++ src/main/java/com/example/musicshop/MainActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public int quantity = 0;
     Spinner spinner;
@@ -27,7 +28,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.createSpinner();
+        this.createMap();
+    }
 
+    void createSpinner() {
         spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
         spinnerArrayList = new ArrayList();
@@ -39,7 +44,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
+    }
 
+    void createMap() {
         goodsMap = new HashMap();
         goodsMap.put("guitar", 500.0);
         goodsMap.put("drums", 1500.0);
@@ -58,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void decreaseQuantity(View view) {
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         this.quantity--;
-        if(this.quantity < 0){
+        if (this.quantity < 0) {
             this.quantity = 0;
         }
         quantityTextView.setText(String.valueOf(this.quantity));
@@ -70,10 +77,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         goodsName = spinner.getSelectedItem().toString();
-        price = (double)goodsMap.get(goodsName);
+        price = (double) goodsMap.get(goodsName);
 
         TextView priceTextView = findViewById(R.id.priceTextView);
         priceTextView.setText("" + price * quantity);
+
+        ImageView goodsImageView = findViewById(R.id.goodsImageView);
+
+        switch (goodsName) {
+            case "guitar":
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+            case "drums":
+                goodsImageView.setImageResource(R.drawable.drums);
+                break;
+            case "keyboard":
+                goodsImageView.setImageResource(R.drawable.keyboard);
+                break;
+            default:
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+        }
     }
 
     @Override
